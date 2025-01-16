e93a6afedd08171988588fcbc4d5f4beac2ec93a
David Granger <dpgranger8@gmail.com>
2025-01-15 19:00:40 -0700

    added sticky header and html->swiftui conversions

--- mihealth.js
+++ mihealth.js
@@ -2,7 +2,19 @@ const navLinks = document.querySelectorAll('.nav-links a')
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
-        navLinks.forEach(link => link.classList.remove('active'));
+        removeActive();
         link.classList.add('active');
     });
 });
+
+const mainLinks = document.querySelectorAll('.logo span')
+
+mainLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        removeActive();
+    });
+});
+
+function removeActive() {
+    navLinks.forEach(link => link.classList.remove('active'));
+}
